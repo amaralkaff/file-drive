8e95a63902f2de41951677c0608217330f9f0f42
amaralkaff <tpamangly@gmail.com>
2024-03-29 04:01:18 +0700

    Fix useQuery options in page.tsx and update publicRoutes in middleware.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import { api } from "../../convex/_generated/api";
 import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization } from "@clerk/nextjs";
@@ -8,7 +8,7 @@ export default function Page() {
   const { organization } = useOrganization();
   console.log(organization?.id);
   const createFile = useMutation(api.files.createFile);
-  const files = useQuery(api.files.getFiles);
+  const files = useQuery(api.files.getFiles, { name: "Hello World" });
   return (
     <main className="flex flex-col items-center justify-center w-screen h-screen space-y-4">
       <h1 className="text-4xl font-bold">Welcome to Drive-mangLy</h1>
