0895ec9060bd80a778579ccc97e492c56abf8040
amaralkaff <tpamangly@gmail.com>
2024-03-29 19:36:22 +0700

    Add access control to file creation

--- convex/files.ts
+++ convex/files.ts
@@ -1,5 +1,18 @@
-import { mutation, query } from './_generated/server'
+import { log } from 'console';
+import { MutationCtx, QueryCtx, mutation, query } from './_generated/server'
 import { ConvexError, v } from "convex/values"
+import { getUser } from './users';
+
+async function hasAccessToOrg(ctx: QueryCtx | MutationCtx, tokenIdentifier: string, orgId: string) {
+    const user = await getUser(ctx, tokenIdentifier);
+        
+        const hasAcces = user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId);
+
+        if (!hasAcces) {
+            throw new ConvexError("You do not have permission to create a file in this organization")
+        }
+        return hasAcces;
+}
 
 export const createFile = mutation({
     args: {
@@ -8,11 +21,17 @@ export const createFile = mutation({
     },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
+        console.log(identity, "identity in createFile")
+        
         if (!identity) {
             throw new ConvexError("You must be signed in to create a file")
         }
 
-        identity.tokenIdentifier
+        const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, args.orgId);
+
+        if (!hasAccess) {
+            throw new ConvexError("You do not have permission to create a file in this organization")
+        }
 
         await ctx.db.insert("files", {
             name: args.name,
@@ -27,10 +46,17 @@ export const getFiles = query({
     },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
-
+        
         if (!identity) {
            return [];
         }
+
+        const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, args.orgId);
+
+        if (!hasAccess) {
+            return [];
+        }
+        
         return ctx.db.query("files").withIndex("by_orgId", q => 
             q.eq("orgId", args.orgId)
         ).collect();
--- src/app/header.tsx
+++ src/app/header.tsx
@@ -3,16 +3,16 @@ import { OrganizationSwitcher, SignInButton, SignedOut, UserButton } from "@cler
 
 export function Header() {
     return (
-        <div className="border-b">
+        <div className="border-b border-gray-200">
             <div className="container mx-auto justify-between flex items-center py-4 bg-gray-50">
                 <div>
                     <a href="/" className="text-lg font-bold">Drive-MangLy</a>
                 </div>
-                <div className="flex gap-2">
+                <div className="flex gap-2 items-center">
                     <OrganizationSwitcher />
                     <UserButton />
                     <SignedOut>
-                        <SignInButton>
+                        <SignInButton mode="modal">
                             <Button>SignIn</Button>
                         </SignInButton>
                     </SignedOut>
