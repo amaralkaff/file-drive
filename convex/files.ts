9037f141d11632e157ec6bdc010478282e27b6bf
amaralkaff <tpamangly@gmail.com>
2024-03-30 16:21:45 +0700

    Refactor schema and layout components, add new dependencies

--- convex/files.ts
+++ convex/files.ts
@@ -3,6 +3,16 @@ import { MutationCtx, QueryCtx, mutation, query } from './_generated/server'
 import { ConvexError, v } from "convex/values"
 import { getUser } from './users';
 
+export const generateUploadUrl = mutation(async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+        
+        if (!identity) {
+            throw new ConvexError("You must be signed in to create a file")
+        }
+
+  return await ctx.storage.generateUploadUrl();
+});
+
 async function hasAccessToOrg(ctx: QueryCtx | MutationCtx, tokenIdentifier: string, orgId: string) {
     const user = await getUser(ctx, tokenIdentifier);
         
@@ -17,11 +27,11 @@ async function hasAccessToOrg(ctx: QueryCtx | MutationCtx, tokenIdentifier: stri
 export const createFile = mutation({
     args: {
         name: v.string(),
+        fileId: v.id("_storage"),
         orgId: v.string(),
     },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
-        console.log(identity, "identity in createFile")
         
         if (!identity) {
             throw new ConvexError("You must be signed in to create a file")
@@ -36,6 +46,7 @@ export const createFile = mutation({
         await ctx.db.insert("files", {
             name: args.name,
             orgId: args.orgId,
+            fileId: args.fileId,
         })
     }
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,163 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { api } from "../../convex/_generated/api";
-import { SignInButton, SignOutButton, SignedIn, SignedOut, useOrganization, useUser } from "@clerk/nextjs";
+import { 
+  SignInButton, 
+  SignOutButton, 
+  SignedIn, 
+  SignedOut, 
+  useOrganization, 
+  useUser 
+} from "@clerk/nextjs";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 import { useMutation, useQuery } from "convex/react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { set, useForm } from "react-hook-form"
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
 
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((value) => value instanceof FileList, "File is required")
+    .refine(value => value.length > 0, "File is required")
+})
 
 export default function Page() {
+  const { toast } = useToast();
   const organization = useOrganization();
   const user = useUser()
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  })
+
+  const fileRef = form.register("file");
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!orgId) return;
+
+    const postUrl = await generateUploadUrl();
+
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": values.file[0]!.type },
+      body: values.file[0],
+    });
+    const { storageId } = await result.json();
+
+    await createFile({
+      name: values.title,
+      fileId: storageId,
+      orgId
+    });
+
+    form.reset();
+
+    setIsFileDialogOpen(false);
+
+    toast({
+      variant: "default",
+      title: "File uploaded",
+      description: "Your file has been uploaded",
+    })
+  }
+
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
   }
+  const [isFileDialogOpen, setIsFileDialogOpen] = useState(false);
+
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
   return (
-    <main className="flex flex-col items-center justify-center w-screen h-screen space-y-4">
-      <h1 className="text-4xl font-bold">Welcome to Drive-mangLy</h1>
-      {files?.map(file => (
-        <div key={file._id}>{file.name}</div>
-      ))}
+    <main className="container mx-auto py-8">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold">Your files</h1>
+        <Dialog open={isFileDialogOpen} onOpenChange={setIsFileDialogOpen}>
+          <DialogTrigger asChild>
+            <Button onClick={() => {
+            }
+            }>Upload file</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-8">Upload file</DialogTitle>
+              <DialogDescription>
+                <Form {...form}>
+                  <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Title</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
 
-      {files?.length === 0 && (
-        <div>No files found</div>
-      )}
-
-      <Button onClick={() => {
-        console.log(organization, "orgId", orgId);
-        if (!orgId) {
-          return;
-        }
-        createFile({ name: "Hello World", orgId })
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={() => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="file" {...fileRef}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+      {
+        files?.map(file => (
+          <div key={file._id}>{file.name}</div>
+        ))
       }
-      }>Create file</Button>
-    </main>
+
+      {
+        files?.length === 0 && (
+          <div>No files found</div>
+        )
+      }
+    </main >
   )
 }
